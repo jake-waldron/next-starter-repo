bc21d8ce3208e61c78ab930c5bd70c4d9a8ebc0e
Jake Waldron <jakewaldron@gmail.com>
2023-03-19 14:12:39 -0400

    setup prisma

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,10 @@
+import type { User } from "@prisma/client";
 import Head from "next/head";
 import Link from "next/link";
-export default function Home() {
+
+import prisma from "@/lib/prisma";
+
+export default function Home({ users }: { users: User[] }) {
   return (
     <>
       <Head>
@@ -11,7 +15,24 @@ export default function Home() {
       <main className="flex h-screen w-full flex-col items-center justify-center">
         <h1>Hello World!</h1>
         <Link href="/about">About</Link>
+        <div className="w-1/2">
+          {users.map((user) => (
+            <div key={user.id} className="flex justify-between">
+              <h2>{user.name}</h2>
+              <p>{user.email}</p>
+            </div>
+          ))}
+        </div>
       </main>
     </>
   );
+}
+
+export async function getServerSideProps() {
+  const users = await prisma.user.findMany({});
+  return {
+    props: {
+      users,
+    },
+  };
 }
